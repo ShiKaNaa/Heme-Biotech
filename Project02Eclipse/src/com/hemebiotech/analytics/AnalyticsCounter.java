dfb6a282f00614a053ad29cccb726f7d81f64353
Raphael Houdry <raphael-59200@hotmail.fr>
2022-11-01 10:18:51 +0100

    changed HashMap to Treemap to order alphabeticaly

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,15 +6,15 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	
 	static File documentToParse = new File("symptoms.txt");
 	
 	static List<String> listOfSymptoms = new ArrayList<String>();
-	static HashMap<String, Integer> symptomeFrequencyMap = new HashMap<String, Integer>();
+	static TreeMap<String, Integer> symptomeFrequencyMap = new TreeMap<String, Integer>();
 	
 	static FileWriter resultsDocument;
 	
@@ -52,7 +52,7 @@ public class AnalyticsCounter {
 	 * It will check if a symptom is already in the HashMap as a key
 	 * If true it will add 1 to the counter, else it will create the key and initialize it at 1
 	 */
-	public static void countingSymptoms(HashMap<String, Integer> symptomeFrequencyMap, List<String> listOfSymptoms) {
+	public static void countingSymptoms(TreeMap<String, Integer> symptomeFrequencyMap, List<String> listOfSymptoms) {
 		
 		// Iterate over the list of symptoms
 		for(String symptom : listOfSymptoms) {
@@ -72,7 +72,7 @@ public class AnalyticsCounter {
 	 * @param symptomeFrequencyMap(HashMap String - Integer)
 	 * @throws IOException
 	 */
-	public static void writingFile(FileWriter resultsDocument, HashMap<String, Integer> symptomeFrequencyMap) throws IOException{
+	public static void writingFile(FileWriter resultsDocument, TreeMap<String, Integer> symptomeFrequencyMap) throws IOException{
 		for(String symptom : symptomeFrequencyMap.keySet()) {
 			try {
 				resultsDocument.write(symptom + ": "  + symptomeFrequencyMap.get(symptom) +  "\n");
